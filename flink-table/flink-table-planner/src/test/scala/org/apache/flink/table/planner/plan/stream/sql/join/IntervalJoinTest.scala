efbd74832d8c13ce296e80d0e5c386ac21ba2c6d
Weiqing Yang <wiyang@linkedin.com>
2026-06-07 00:16:13 -0700

    [FLINK-36953][table] Add EARLY_FIRE join hint surface and planner plumbing
    Introduce the EARLY_FIRE('delay'=..., 'time_mode'=...) SQL join hint for
    interval joins and thread it through the planner as inert metadata.
    
    EarlyFireJoinHintOptions defines the typed option keys: a required 'delay'
    duration and an optional 'time_mode' (rowtime|proctime). The hint is
    registered in FlinkHintStrategies with a key-value option checker that
    requires a positive delay, validates time_mode, and rejects unknown option
    keys. JoinStrategy, CapitalizeQueryHintsShuttle, and QueryHintsResolver are
    extended so the hint propagates intact to StreamPhysicalIntervalJoinRule,
    which resolves the default time_mode from the window bounds and rejects
    time_mode=rowtime on a proctime join (and, for now, time_mode=proctime on an
    event-time join).
    
    The resolved delay and time_mode are carried on StreamPhysicalIntervalJoin
    and serialized as two additive @JsonInclude(NON_NULL) fields on
    StreamExecIntervalJoin, leaving the ExecNode metadata version unchanged so
    existing compiled plans restore as before. No runtime behavior changes yet:
    the operator ignores the new fields.

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/plan/stream/sql/join/IntervalJoinTest.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/plan/stream/sql/join/IntervalJoinTest.scala
@@ -395,6 +395,104 @@ class IntervalJoinTest extends TableTestBase {
     util.verifyExecPlan(sqlQuery)
   }
 
+  // Tests for the EARLY_FIRE join hint
+  @Test
+  def testEarlyFireOnRowTimeLeftOuterJoin(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='5s') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    util.verifyExecPlan(sqlQuery)
+  }
+
+  @Test
+  def testEarlyFireMissingDelay(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('time_mode'='rowtime') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasMessageContaining("incomplete required option(s)")
+  }
+
+  @Test
+  def testEarlyFireNonPositiveDelay(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='0s') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasMessageContaining("value should be a positive duration")
+  }
+
+  @Test
+  def testEarlyFireInvalidTimeMode(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='5s', 'time_mode'='unknown') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasMessageContaining("Invalid EARLY_FIRE hint options")
+  }
+
+  @Test
+  def testEarlyFireUnknownOption(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='5s', 'timemode'='proctime') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasMessageContaining("Unsupported EARLY_FIRE hint option(s) [timemode]")
+  }
+
+  @Test
+  def testEarlyFireRowTimeOnProcTimeJoin(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='5s', 'time_mode'='rowtime') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.proctime BETWEEN t2.proctime - INTERVAL '1' HOUR AND t2.proctime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasStackTraceContaining("requires a row-time interval join")
+  }
+
+  @Test
+  def testEarlyFireProcTimeOnRowTimeJoin(): Unit = {
+    val sqlQuery =
+      """
+        |SELECT /*+ EARLY_FIRE('delay'='5s', 'time_mode'='proctime') */ t1.a, t2.b
+        |FROM MyTable t1 LEFT OUTER JOIN MyTable2 t2 ON
+        |  t1.a = t2.a AND
+        |  t1.rowtime BETWEEN t2.rowtime - INTERVAL '10' SECOND AND t2.rowtime + INTERVAL '1' HOUR
+      """.stripMargin
+
+    assertThatThrownBy(() => util.verifyExecPlan(sqlQuery))
+      .hasStackTraceContaining("not yet supported")
+  }
+
   // Other tests
   @Test
   def testJoinTimeBoundary(): Unit = {
